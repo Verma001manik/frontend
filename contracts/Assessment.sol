f9413772efb72f446fbf8caa5619a1bdd96fa653
Manik Verma <verma001manik@gmail.com>
2023-07-21 14:11:52 +0530

    fuck

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,89 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
-    address payable public owner;
+    address public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event DonationReceived(address sender, uint256 amount);
+    event FundsTransferred(address sender, address recipient, uint256 amount);
+    event BillPaid(address indexed payer, uint256 indexed billId, uint256 amount);
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
+    constructor(uint256 initBalance) payable {
+        owner = msg.sender;
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can perform this action");
+        _;
+    }
+
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
+    function withdraw(uint256 _withdrawAmount) public onlyOwner {
+        require(balance >= _withdrawAmount, "Insufficient contract balance");
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function increaseBalance(uint256 amount) public onlyOwner {
+        balance += amount;
+        emit Deposit(amount);
+    }
+
+    function decreaseBalance(uint256 amount) public onlyOwner {
+        require(balance >= amount, "Cannot decrease balance by more than the current balance");
+        balance -= amount;
+        emit Withdraw(amount);
+    }
+
+    function transferFunds(address payable recipient, uint256 amount) public onlyOwner {
+        require(recipient != address(0), "Invalid recipient address");
+        require(amount <= balance, "Insufficient contract balance");
+        recipient.transfer(amount);
+        emit FundsTransferred(msg.sender, recipient, amount);
+    }
+
+    function payBill(uint256 billId) public payable {
+        require(billId >= 0 && billId < 3, "Invalid billId"); // Assuming there are 3 bills to pay
+        uint256 billAmount = getBillAmount(billId);
+        require(msg.value == billAmount, "Incorrect payment amount");
+        address payable billRecipient = getBillRecipient(billId);
+        billRecipient.transfer(msg.value);
+        emit BillPaid(msg.sender, billId, msg.value);
+        balance -= billAmount;
+    }
+
+    // Helper functions for bills (You can customize these as per your requirements)
+    function getBillAmount(uint256 billId) public pure returns (uint256) {
+        if (billId == 0) {
+            return 0.5 ether; // Electric Bill (0.5 ETH)
+        } else if (billId == 1) {
+            return 0.3 ether; // Internet Bill (0.3 ETH)
+        } else if (billId == 2) {
+            return 1 ether; // Rent (1 ETH)
+        }
+        revert("Invalid billId");
+    }
+
+    function getBillRecipient(uint256 billId) public view returns (address payable) {
+        if (billId == 0) {
+            return payable(0x1111111111111111111111111111111111111111); // Replace with the Electric Bill recipient address
+        } else if (billId == 1) {
+            return payable(0x2222222222222222222222222222222222222222); // Replace with the Internet Bill recipient address
+        } else if (billId == 2) {
+            return payable(0x3333333333333333333333333333333333333333); // Replace with the Rent recipient address
+        }
+        revert("Invalid billId");
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
@@ -7,41 +7,36 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [selectedBillId, setSelectedBillId] = useState(null);
+  const [billAmount, setBillAmount] = useState(0);
+  const [transactionSuccess, setTransactionSuccess] = useState(false);
+  const [recipientAddress, setRecipientAddress] = useState("");
+  const [transferAmount, setTransferAmount] = useState("");
 
-  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const contractAddress = "0x82e01223d51Eb87e16A03E24687EDF0F294da6f1";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async() => {
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
-      handleAccount(account);
+      const accounts = await ethWallet.request({ method: "eth_accounts" });
+      setAccount(accounts[0]);
     }
-  }
+  };
 
-  const handleAccount = (account) => {
-    if (account) {
-      console.log ("Account connected: ", account);
-      setAccount(account);
-    }
-    else {
-      console.log("No account found");
-    }
-  }
-
-  const connectAccount = async() => {
+  const connectAccount = async () => {
     if (!ethWallet) {
       alert('MetaMask wallet is required to connect');
       return;
     }
-  
+
     const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
-    handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
+    setAccount(accounts[0]);
+
+    // Once the wallet is set, get a reference to our deployed contract
     getATMContract();
   };
 
@@ -49,63 +44,134 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
+
     setATM(atmContract);
-  }
+  };
 
-  const getBalance = async() => {
+  const getBalance = async () => {
     if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
+      const balanceBigNumber = await atm.getBalance();
+      setBalance(balanceBigNumber.toString());
     }
-  }
+  };
 
-  const deposit = async() => {
+  const deposit = async () => {
     if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
+      const tx = await atm.deposit(ethers.utils.parseEther("1"));
+      await tx.wait();
       getBalance();
     }
-  }
+  };
 
-  const withdraw = async() => {
+  const withdraw = async () => {
     if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
+      const tx = await atm.withdraw(ethers.utils.parseEther("1"));
+      await tx.wait();
       getBalance();
+    }
+  };
+
+  async function transferFunds(recipientAddress, amount) {
+    try {
+      const contractBalance = await atm.getBalance();
+      if (amount <= contractBalance) {
+        const tx = await atm.transferFunds(
+          ethers.utils.getAddress(recipientAddress),
+          ethers.utils.parseEther(amount.toString())
+        );
+        await tx.wait();
+        setTransactionSuccess(true); // Set transaction success message
+        getBalance(); // Update balance after successful transfer
+      } else {
+        console.error("Insufficient contract balance");
+      }
+    } catch (error) {
+      console.error("Error transferring funds:", error);
     }
   }
 
-  const initUser = () => {
-    // Check to see if user has Metamask
-    if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
-    }
+  const handleBillSelect = (billId, amount) => {
+    setSelectedBillId(billId);
+    setBillAmount(amount);
+    setTransactionSuccess(false); // Reset transaction success message
+  };
 
-    // Check to see if user is connected. If not, connect to their account
-    if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
-    }
+  const payBill = async () => {
+    if (atm && parseFloat(billAmount) > 0) {
+      try {
+        // Send the transaction to pay the selected bill
+        const tx = await atm.payBill(selectedBillId, { value: ethers.utils.parseEther(billAmount.toString()) });
+        await tx.wait();
 
-    if (balance == undefined) {
-      getBalance();
-    }
+        // Update balance after successful payment
+        getBalance();
 
-    return (
-      <div>
-        <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
-      </div>
-    )
+        // Show transaction success message
+        setTransactionSuccess(true);
+      } catch (error) {
+        console.error("Error paying bill:", error);
+      }
+    }
+  };
+
+  useEffect(() => { getWallet(); }, []);
+
+  if (!ethWallet) {
+    return <p>Please install Metamask in order to use this ATM.</p>;
   }
 
-  useEffect(() => {getWallet();}, []);
+  if (!account) {
+    return <button onClick={connectAccount}>Please connect your Metamask wallet</button>;
+  }
+
+  if (balance === undefined) {
+    getBalance();
+  }
 
   return (
     <main className="container">
       <header><h1>Welcome to the Metacrafters ATM!</h1></header>
-      {initUser()}
+      <div>
+        <p>Your Account: {account}</p>
+        <p>Your Balance: {balance} ETH</p>
+        <button onClick={deposit}>Deposit 1 ETH</button>
+        <button onClick={withdraw}>Withdraw 1 ETH</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Recipient Address"
+          onChange={(e) => setRecipientAddress(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Amount to Transfer"
+          onChange={(e) => setTransferAmount(e.target.value)}
+        />
+        <button onClick={() => transferFunds(recipientAddress, transferAmount)}>Transfer Funds</button>
+      </div>
+      <div>
+        <h2>Pay Bills</h2>
+        <ul>
+          <li>
+            <button onClick={() => handleBillSelect(0, 0.5)}>Pay Electric Bill (0.5 ETH)</button>
+          </li>
+          <li>
+            <button onClick={() => handleBillSelect(1, 0.3)}>Pay Internet Bill (0.3 ETH)</button>
+          </li>
+          <li>
+            <button onClick={() => handleBillSelect(2, 1)}>Pay Rent (1 ETH)</button>
+          </li>
+        </ul>
+        {selectedBillId !== null && (
+          <div>
+            <p>Selected Bill: {selectedBillId}</p>
+            <p>Amount: {billAmount} ETH</p>
+            <button onClick={payBill}>Pay Bill</button>
+            {transactionSuccess && <p>Bill payment successful!</p>}
+          </div>
+        )}
+      </div>
       <style jsx>{`
         .container {
           text-align: center
@@ -113,5 +179,5 @@ export default function HomePage() {
       `}
       </style>
     </main>
-  )
+  );
 }
